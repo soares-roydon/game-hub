2d02e89e4b72e32d36709e8d4c37b3ad80994278
soares-roydon <soaresroydon@gmail.com>
2026-05-27 18:23:02 +0530

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
   <>
-  <div className=" min-h-screen grid md:grid-cols-[250px_1fr]">
+  <NavBar />
+  <div className="grid md:grid-cols-[250px_1fr]">
     <div className="hidden md:block md:bg-blue-500">sidebar</div>
     <div className="bg-red-500">Body</div>
   </div>
