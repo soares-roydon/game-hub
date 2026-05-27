8c7934de90362b8d2ae03f5993171f9fdad81c99
soares-roydon <soaresroydon@gmail.com>
2026-05-27 17:32:20 +0530

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
   <>
-    <NavBar />
+  <div className=" min-h-screen grid md:grid-cols-[250px_1fr]">
+    <div className="hidden md:block md:bg-blue-500">sidebar</div>
+    <div className="bg-red-500">Body</div>
+  </div>
   </>
   )
 }
